d6c4d664de4b7e745f2bd81f7e13093dd3ec1ae0
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-09-21 21:05:33 -0300

    Create aula39.py

---
+++ aula39.py
@@ -0,0 +1,11 @@
+nome = input('Digite seu nome: ')
+
+contador = 0
+novo_nome = ''
+while contador <  len(nome):
+    letra = nome[contador]
+    novo_nome += f'*{letra}'
+    contador += 1
+
+novo_nome += '*'
+print(novo_nome)
