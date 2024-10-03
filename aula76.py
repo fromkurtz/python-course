a77b5034ed469a526a35b43a465e78e315aecaa6
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-10-03 19:11:26 -0300

    Update aula76.py

--- aula76.py
+++ aula76.py
@@ -1,41 +1,25 @@
-# Dicionários em Python (tipo dict)
-# Dicionários são estruturas de dados do tipo
-# par de "chave" e "valor".
-# Chaves podem ser consideradas como o "índice"
-# que vimos na lista e podem ser de tipos imutáveis
-# como: str, int, float, bool, tuple, etc.
-# O valor pode ser de qualquer tipo, incluindo outro
-# dicionário.
-# Usamos as chaves - {} - ou a classe dict para criar
-# dicionários.
-# Imutáveis: str, int, float, bool, tuple
-# Mutável: dict, list
-# pessoa = {
-#     'nome': 'Luiz Otávio',
-#     'sobrenome': 'Miranda',
-#     'idade': 18,
-#     'altura': 1.8,
-#     'endereços': [
-#         {'rua': 'tal tal', 'número': 123},
-#         {'rua': 'outra rua', 'número': 321},
-#     ]
-# }
-# pessoa = dict(nome='Luiz Otávio', sobrenome='Miranda')
-pessoa = {
-    'nome': 'Luiz Otávio',
-    'sobrenome': 'Miranda',
-    'idade': 18,
-    'altura': 1.8,
-    'endereços': [
-        {'rua': 'tal tal', 'número': 123},
-        {'rua': 'outra rua', 'número': 321},
-    ],
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+import copy
+
+d1 = {
+    'c1': 1,
+    'c2': 2,
+    'l1': [0, 1, 2],
 }
-# print(pessoa, type(pessoa))
-print(pessoa['nome'])
-print(pessoa['sobrenome'])
+d2 = d1.copy()
 
-print()
+d2['c1'] = 1000
+d2['l1'][1] = 999999
 
-for chave in pessoa:
-    print(chave, pessoa[chave])
+print(d1)
+print(d2)
