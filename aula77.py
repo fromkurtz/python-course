de6337cad669b00b4b44c0a5d3bbdfd3b69b904f
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-10-06 19:55:42 -0300

    Update aula77.py

--- aula77.py
+++ aula77.py
@@ -1,25 +1,58 @@
-# Manipulando chaves e valores em dicionários
-pessoa = {}
 
-##
-##
-
-chave = 'nome'
-
-pessoa[chave] = 'Luiz Otávio'
-pessoa['sobrenome'] = 'Miranda'
+# Exercício - sistema de perguntas e respostas
 
 
-print(pessoa[chave])
+perguntas = [
+    {
+        'Pergunta': 'Quanto é 2+2?',
+        'Opções': ['1', '3', '4', '5'],
+        'Resposta': '4',
+    },
+    {
+        'Pergunta': 'Quanto é 5*5?',
+        'Opções': ['25', '55', '10', '51'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10/2?',
+        'Opções': ['4', '5', '2', '1'],
+        'Resposta': '5',
+    },
+]
 
-pessoa[chave] = 'Maria'
+qtd_acertos = 0
+for pergunta in perguntas:
+    print('Pergunta:', pergunta['Pergunta'])
+    print()
 
-del pessoa['sobrenome']
-print(pessoa)
-print(pessoa['nome'])
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
+    print()
 
-# print(pessoa.get('sobrenome'))
-if pessoa.get('sobrenome') is None:
-    print('NÃO EXISTE')
-else:
-    print(pessoa['sobrenome'])
+    escolha = input('Escolha uma opção: ')
+
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+
+    print()
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou 👍')
+    else:
+        print('Errou ❌')
+
+    print()
+
+
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas.')
