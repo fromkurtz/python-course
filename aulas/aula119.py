c24706e8419be3c8dfa118dcc63c70a155b91cb9
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-11-11 17:55:00 -0300

    Update aula119.py

---
+++ aulas/aula119.py
@@ -0,0 +1,15 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+# Música para codar =)
+# Everybody wants to rule the world - Tears for fears
+# todo = [] -> lista de tarefas
+# todo = ['fazer café'] -> Adicionar fazer café
+# todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
+# desfazer = ['fazer café',] -> Refazer ['caminhar']
+# desfazer = [] -> Refazer ['caminhar', 'fazer café']
+# refazer = todo ['fazer café']
+# refazer = todo ['fazer café', 'caminhar']
+import os
+
+caminho_arquivo = 'lista_de_tarefas.txt'
+
+with open(caminho_arquivo, )
