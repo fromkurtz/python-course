a3e22c9ddf70ce9d32ea1ee6e7e27cd2c23b43ac
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-11-24 11:36:02 -0300

    novas aulas

--- aulas/aula126_a.py
+++ aulas/aula126_a.py
@@ -10,3 +10,12 @@ class Pessoa:
 p1 = Pessoa('Joao', 33)
 p2 = Pessoa('Helena', 12)
 p3 = Pessoa('Joana', 53)
+bd = [vars(p1), p2.__dict__, vars(p3)]
+
+def fazer_dump():
+    with open(CAMINHO_JSON, 'w') as arquivo:
+        json.dump(bd, arquivo, ensure_ascii=False, indent=2)
+
+if __name__ == '__main__':
+    print('ELE E O __main__')
+    fazer_dump()
