1c69a48791381d123b71ba9e62c991e779f869e4
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-11-26 16:04:07 -0300

    Update aula139.py

--- aulas/aula139.py
+++ aulas/aula139.py
@@ -14,15 +14,22 @@
 # string = MinhaString('Luiz')
 # print(string.upper())
 
-class A:
+class A(object):
     atributo_a = 'valor a'
+
+    def __init__(self, atributo):
+        self.atributo = atributo
 
     def metodo(self):
         print('A')
-    
-    
+
+
 class B(A):
-    atributo_b = 'valor b '
+    atributo_b = 'valor b'
+
+    def __init__(self, atributo, outra_coisa):
+        super().__init__(atributo)
+        self.outra_coisa = outra_coisa
 
     def metodo(self):
         print('B')
@@ -31,12 +38,27 @@ class B(A):
 class C(B):
     atributo_c = 'valor c'
 
+    def __init__(self, *args, **kwargs):
+        # print('EI, burlei o sistema.')
+        super().__init__(*args, **kwargs)
+
     def metodo(self):
-        super().metodo()
+        # super().metodo()  # B
+        # super(B, self).metodo()  # A
+        # super(A, self).metodo()  # object
+        A.metodo(self)
+        B.metodo(self)
         print('C')
 
-print(C.mro())
-# c = C()
+
+# print(C.mro())
+# print(B.mro())
+# print(A.mro())
+c = C('Atributo', 'Qualquer')
+# print(c.atributo)
+# print(c.outra_coisa)
+c.metodo()
 # print(c.atributo_a)
 # print(c.atributo_b)
 # print(c.atributo_c)
+# c.metodo()
