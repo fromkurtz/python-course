cf522ccc781b877eef60f5bd6728a6667a1cd67f
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-12-03 19:01:15 -0300

    Update aula143.py

---
+++ aulas/aula143.py
@@ -0,0 +1,6 @@
+# abstractmethod para qualquer método já decorado (@property e setter)
+# É possível criar @property @property.setter @classmethod
+# @staticmethod e métodos normais como abstratos, para isso
+# use @abstractmethod como decorator mais interno.
+# Foo - Bar são palavras usadas como placeholder
+# para palavras que podem mudar na programação.
