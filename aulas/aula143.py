a77eaa14d00a1b713bb2bba98b58341fbc27ac78
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-12-03 19:10:03 -0300

    Update aula143.py

--- aulas/aula143.py
+++ aulas/aula143.py
@@ -4,3 +4,29 @@
 # use @abstractmethod como decorator mais interno.
 # Foo - Bar são palavras usadas como placeholder
 # para palavras que podem mudar na programação.
+from abc import ABC, abstractmethod
+
+
+class AbstractFoo(ABC):
+    def __init__(self, name):
+        self._name = None
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+
+
+class Foo(AbstractFoo):
+    def __init__(self, name):
+        super().__init__(name)
+        # print('Sou inutil')
+
+
+foo = Foo('bar')
+print(foo.name)
