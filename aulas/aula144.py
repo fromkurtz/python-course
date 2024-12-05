10977d670c73249c4a24970dd826a9f654cded08
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-12-05 10:34:11 -0300

    Update aula144.py

--- aulas/aula144.py
+++ aulas/aula144.py
@@ -11,3 +11,40 @@
 # Princípio da substituição de liskov
 # Objetos de uma superclasse devem ser substituíveis
 # por objetos de uma subclasse sem quebrar a aplicação.
+from abc import ABC, abstractmethod
+
+
+class Notificacao(ABC):
+    def __init__(self, mensagem) -> None:
+        self.mensagem = mensagem
+
+    @abstractmethod
+    def enviar(self) -> bool: ...
+
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self) -> bool:
+        print('E-mail: enviando - ', self.mensagem)
+        return True
+
+
+class NotificacaoSMS(Notificacao):
+    def enviar(self) -> bool:
+        print('SMS: enviando - ', self.mensagem)
+        return False
+
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+
+    if notificacao_enviada:
+        print('Notificacao enviada')
+    else:
+        print('Notificacao NAO enviada')
+
+
+notificacao_email = NotificacaoEmail('testando e-mail')
+notificar(notificacao_email)
+
+notificacao_sms = NotificacaoSMS('testando SMS')
+notificar(notificacao_sms)
