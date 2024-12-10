dc3c9fcf517755bbf57d34495d630c8ed800655d
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-12-10 10:10:12 -0300

    Update aula152.py

--- aulas/aula152.py
+++ aulas/aula152.py
@@ -4,9 +4,13 @@ def meu_repr(self):
     class_dict = self.__dict__
     class_repr = f'{class_name}({class_dict})'
     return class_repr
+
+
 def adiciona_repr(cls):
     cls.__repr__ = meu_repr
     return cls
+
+
 def meu_planeta(metodo):
     def interno(self, *args, **kwargs):
         resultado = metodo(self, *args, **kwargs)
@@ -14,24 +18,35 @@ def meu_planeta(metodo):
             return 'Você está em casa'
         return resultado
     return interno
+
+
 @adiciona_repr
 class Time:
     def __init__(self, nome):
         self.nome = nome
+
 @adiciona_repr
 class Planeta:
     def __init__(self, nome):
         self.nome = nome
+
+
     @meu_planeta
     def falar_nome(self):
         return f'O planeta é {self.nome}'
+    
+
 brasil = Time('Brasil')
 portugal = Time('Portugal')
+
 terra = Planeta('Terra')
 marte = Planeta('Marte')
+
 print(brasil)
 print(portugal)
+
 print(terra)
 print(marte)
+
 print(terra.falar_nome())
 print(marte.falar_nome())
