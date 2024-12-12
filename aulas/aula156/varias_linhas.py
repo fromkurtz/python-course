6f59da980e7a1a327acb8c77c8b439578d135631
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-12-12 15:33:50 -0300

    descricao doc string

---
+++ aulas/aula156/varias_linhas.py
@@ -0,0 +1,39 @@
+"""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin maximus,
+libero sed viverra mollis, augue elit cursus ipsum,
+quis gravida augue metus nec nunc. Vestibulum ante ipsum primis in faucibus
+orci luctus et ultrices posuere cubilia curae; Vivamus bibendum ex ac lectus
+mollis porttitor. Duis ac tortor cursus, aliquet quam sed, elementum enim.
+Phasellus iaculis volutpat dui non gravida. Etiam maximus convallis sem
+sed pharetra. Curabitur varius bibendum egestas. Suspendisse molestie,
+urna sit amet venenatis eleifend, ante nibh facilisis diam, non euismod tellus
+turpis a tortor.
+
+Sed lacus leo, volutpat id malesuada in, feugiat non ex. Sed eget purus varius,
+ullamcorper orci et, dignissim nulla. Nam ultrices semper nisi sed tempus.
+Nulla eget elit ante. Pellentesque metus quam, blandit sed tempus at,
+dictum eu tortor. Proin pharetra lacus in mauris facilisis placerat.
+Aliquam vulputate imperdiet risus. Fusce luctus dolor nisi, a aliquet ex
+interdum ac. In ac sapien suscipit, lobortis metus sed, pulvinar ex.
+
+Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent ac
+turpis finibus, aliquam urna in, laoreet ex. Aenean tempus purus leo,
+et blandit mauris sagittis vitae. Morbi feugiat eget diam quis pulvinar. Mauris
+ut varius augue. Nulla facilisi. Mauris mollis ante quam, dignissim sollicitudin
+metus euismod ac. Sed accumsan rutrum sollicitudin. Aliquam non suscipit ipsum,
+nec consectetur lacus. Maecenas et mauris velit. Aenean efficitur dictum neque,
+quis ultrices nisl. Cras at eros non ligula dignissim semper. Nam massa ex,
+dapibus non tempor et, efficitur ut orci. Nunc suscipit et turpis eget finibus.
+Aliquam non ligula diam.
+
+Sed vulputate pellentesque tortor nec fringilla. Fusce rhoncus odio ac ligula
+venenatis dapibus. Aliquam ultrices gravida velit condimentum euismod. 
+Aliquam erat volutpat. Aenean vestibulum orci ut ligula ultrices eleifend. 
+Morbi leo lectus, aliquam vitae ultricies a, faucibus eget lacus. Etiam 
+venenatis lacus non ornare placerat. Ut id tincidunt risus, quis posuere magna. 
+Sed vel ex quam.
+
+Sed vitae ornare velit. Maecenas in risus consequat, lobortis mi sed, 
+consectetur lorem. Vestibulum vel ultrices metus, non interdum nibh. 
+Aenean molestie viverra tempus. Vestibulum laoreet tellus vel nibh rutrum 
+fermentum. Sed sollicitudin rutrum venenatis. 
+Nulla ut dolor id dui feugiat egestas."""
