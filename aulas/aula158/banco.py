83747ec0521aa785ce7f7563672715f86c3d6c64
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-12-17 17:11:09 -0300

    criando os autenticadores

--- aulas/aula158/banco.py
+++ aulas/aula158/banco.py
@@ -1,5 +1,35 @@
+import contas
+import pessoas
+
 class Banco:
-    def __init__(self, agencias, clientes, contas):
-        self.agencias = agencias
-        self.clientes = clientes
-        self.contas = contas
+    def __init__(
+     self,
+     agencias: list[int] | None = None,
+     clientes: list[pessoas.Pessoaessoa] | None = None,
+     contas: list[contas.Conta] | None = None
+    ):
+        self.agencias = agencias or []
+        self.clientes = clientes or []
+        self.contas = contas or []
+
+
+    def _checa_agencia(self, conta):
+        if conta.agencia in self.agencias:
+            return True
+        return False
+
+
+    def _checa_cliente(self, cliente):
+        if cliente in self.clientes:
+            return True
+        return False
+
+
+    def _checa_conta(self, conta):
+        if conta in self.contas:
+            return True
+        return False
+    
+
+    def autenticar(self, ciente, conta):
+        
