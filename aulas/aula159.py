9a090ad26c3d4ddbc4931dcd39c4cac47d5e508f
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-12-18 12:46:46 -0300

    asdict e astuple em dataclasses

--- aulas/aula159.py
+++ aulas/aula159.py
@@ -6,14 +6,16 @@
 # Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
 
-from dataclasses import dataclass
+from dataclasses import asdict, astuple, dataclass
 
-@dataclass(repr=True)
+@dataclass
 class Pessoa:
     nome: str
     sobrenome: str
 
 if __name__ == '__main__':
-    lista = [Pessoa('A', 'Z'), Pessoa('b', 'y'), Pessoa('c', 'x')]
-    ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
-    print(ordenadas)
+    p1 = Pessoa('Luiz', 'Otávio')
+    print(asdict(p1).keys())
+    print(asdict(p1).values())
+    print(asdict(p1).items())
+    print(astuple(p1)[0])
