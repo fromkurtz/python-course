b1ac899fb273f01c86fc9b54c90d6a2d57352d7d
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2024-12-20 17:10:11 -0300

    datetime

--- aulas/aula162.py
+++ aulas/aula162.py
@@ -11,12 +11,17 @@
 # Instalando o pytz
 # pip install pytz types-pytz
 from datetime import datetime
+from pytz import timezone
 
 
-data_str_data = '2022/04/20 07:49:23'
-data_str_data = '20/04/2022'
-data_str_fmt = '%d/%m/%Y'
+data = datetime.now()
+data_atual = data.timestamp()
+print(datetime.fromtimestamp(data_atual))
+print(data_atual)
+# data_str_data = '2022/04/20 07:49:23'
+# data_str_data = '20/04/2022'
+# data_str_fmt = '%d/%m/%Y'
 
 # data = datetime(2022, 4, 20, 7, 49, 23)
-data = datetime.strptime(data_str_data, data_str_fmt)
-print(data)
+# data = datetime.strptime(data_str_data, data_str_fmt)
+# print(data)
