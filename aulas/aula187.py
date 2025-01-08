c7a5882b2eb478bdaca5d12ea45abb23838a331d
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2025-01-08 17:23:15 -0300

    sys.argv

--- aulas/aula187.py
+++ aulas/aula187.py
@@ -1,2 +1,15 @@
 # sys.argv - Executando arquivos com argumentos no sistema
-# Fonte = Fira Code
+import sys
+
+argumentos = sys.argv
+qtd_argumentos = len(argumentos)
+
+if qtd_argumentos <= 1:
+    print('Voce nao passou argumentos')
+else:
+    try:
+        print(f'Voce passou os argumentos {argumentos[1:]}')
+        print(f'faca alguma coisa com o {argumentos[1]}')
+        print(f'faca outra coisa com o {argumentos[2]}')
+    except IndexError:
+        print('faltam argumentos')
