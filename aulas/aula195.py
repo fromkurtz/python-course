3b3a2aa8da0083e5b590037c1b20627c7cd3ffa9
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2025-01-16 15:47:49 -0300

    jupyter notebook

---
+++ aulas/aula195.py
@@ -0,0 +1,10 @@
+# Jupyter Notebook - Instalacao e test
+
+# Essa linha cria uma variavel a 
+a = 2
+
+# Essa linha tem o valor da variavel `a` em dobro
+b = a * 2 # 4
+
+# Essa linha mostra o valor de `a` + `b`
+print(a + b) # 6
