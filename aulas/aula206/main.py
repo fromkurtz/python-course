8a6ecacfc53f29694b3f8aa208c4b8f98e97968f
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2025-03-28 10:37:36 -0300

    Update .env example and add MySQL connection setup in main.py

--- aulas/aula206/main.py
+++ aulas/aula206/main.py
@@ -2,3 +2,12 @@
  # Doc: https://pymysql.readthedocs.io/en/latest/
  # Pypy: https://pypi.org/project/pymysql/
  # GitHub: https://github.com/PyMySQL/PyMySQL
+import pymysql
+
+connection = pymysql.connect(
+    host='localhost',
+    user='',
+    password='',
+    database='',
+    charset='utf8mb4',
+)
