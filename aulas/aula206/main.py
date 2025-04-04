d0a9ff950961ed3ed2ac24e3356d6f7bc160b1f8
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2025-04-04 11:19:50 -0300

    Enhance data insertion logic in main.py with multiple methods; update table schema and add SELECT functionality

--- aulas/aula206/main.py
+++ aulas/aula206/main.py
@@ -1,42 +1,116 @@
+# PyMySQL - um cliente MySQL feito em Python Puro
+# Doc: https://pymysql.readthedocs.io/en/latest/
+# Pypy: https://pypi.org/project/pymysql/
+# GitHub: https://github.com/PyMySQL/PyMySQL
 import os
-import pymysql
+
 import dotenv
+import pymysql
 
 TABLE_NAME = 'customers'
 
 dotenv.load_dotenv()
-
 
 connection = pymysql.connect(
     host=os.environ['MYSQL_HOST'],
     user=os.environ['MYSQL_USER'],
     password=os.environ['MYSQL_PASSWORD'],
     database=os.environ['MYSQL_DATABASE'],
+    charset='utf8mb4'
 )
 
 with connection:
     with connection.cursor() as cursor:
-        # Cria a tabela se não existir
         cursor.execute(
             f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
-            '  `id` INT NOT NULL AUTO_INCREMENT,'
-            '  `name` VARCHAR(45) NOT NULL,'
-            '  `age` INT NOT NULL,'
-            '  PRIMARY KEY (`id`))'
+            'id INT NOT NULL AUTO_INCREMENT, '
+            'name VARCHAR(50) NOT NULL, '
+            'age INT NOT NULL, '
+            'PRIMARY KEY (id)'
+            ') '
         )
-        
-        # ⚠️ CUIDADO: ISSO APAGA TODOS OS DADOS!
+        # CUIDADO: ISSO LIMPA A TABELA
         cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
     connection.commit()
 
-    with connection.cursor() as cursor:
-        # Inserção 1 (usando tupla)
-        sql = f'INSERT INTO {TABLE_NAME} (`name`, `age`) VALUES (%s, %s)'
-        cursor.execute(sql, ('Lucas', 30))
-        print("Inserido Lucas, 30")
+    # Começo a manipular dados a partir daqui
 
-        # Inserção 2 (usando dicionário)
-        sql = f'INSERT INTO {TABLE_NAME} (`name`, `age`) VALUES (%(name)s, %(age)s)'
-        cursor.execute(sql, {"name": "Le", "age": 50})
-        print("Inserido Le, 50")
+    # Inserindo um valor usando placeholder e um iterável
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(name, age) '
+            'VALUES '
+            '(%s, %s) '
+        )
+        data = ('Luiz', 18)
+        result = cursor.execute(sql, data)
+        # print(sql, data)
+        # print(result)
     connection.commit()
+
+    # Inserindo um valor usando placeholder e um dicionário
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(name, age) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data2 = {
+            "age": 37,
+            "name": "Le",
+        }
+        result = cursor.execute(sql, data2)
+        # print(sql)
+        # print(data2)
+        # print(result)
+    connection.commit()
+
+    # Inserindo vários valores usando placeholder e um tupla de dicionários
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(name, age) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data3 = (
+            {"name": "Sah", "age": 33, },
+            {"name": "Júlia", "age": 74, },
+            {"name": "Rose", "age": 53, },
+        )
+        result = cursor.executemany(sql, data3)
+        # print(sql)
+        # print(data3)
+        # print(result)
+    connection.commit()
+
+    # Inserindo vários valores usando placeholder e um tupla de tuplas
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(name, age) '
+            'VALUES '
+            '(%s, %s) '
+        )
+        data4 = (
+            ("Siri", 22, ),
+            ("Helena", 15, ),
+        )
+        result = cursor.executemany(sql, data4)
+        # print(sql)
+        # print(data4)
+        # print(result)
+    connection.commit()
+
+    # Lendo os valores com SELECT
+    with connection.cursor() as cursor:
+        sql = (
+            f'SELECT * FROM {TABLE_NAME} '
+        )
+        cursor.execute(sql) 
+        data5 = cursor.fetchall()
+
+        for row in data5:
+            print(row)
