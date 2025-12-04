2dd336f513721b618d43bcee55b0fa27054a4a60
fromkurtz <devbryankurtz@gmail.com>
2025-12-04 18:01:06 -0300

    sigleton metodo __call__

--- design patterns/factories/Singleton/singleton_3.py
+++ design patterns/factories/Singleton/singleton_3.py
@@ -1,13 +1,18 @@
 class Meta(type):
-    pass
+    def __call__(cls, *args, **kwargs):
+        return super().__call__(*args, **kwargs)
 
-class Pessoa:
+class Pessoa(metaclass=Meta):
+    def __new__(cls, *args, **kwargs):
+        print('NEW e executado')
+        return super().__new__(cls)
+
     def __init__(self, nome):
+        print('INIT é executado')
         self.nome = nome
 
     def __call__(self, x, y):
         print('call chamado', self.nome, x + y)
 
 p1 = Pessoa("João")
-p1(2, 2)
 print(p1.nome)
